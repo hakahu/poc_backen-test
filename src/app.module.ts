33af36f913753aa65ef7df20a8f417f0eeca624e
Alex Wolf <animatere@gmail.com>
2024-06-24 00:07:49 +0200

    added payment component

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { LoginModule } from './user-login/user-login.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsModule } from './items/items.module';
 import { SignupModule } from './user-signup/user-signup.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [LoginModule, SignupModule, MongooseModule.forRoot('mongodb+srv://backend-test:Test123@backend-test.dayvnzs.mongodb.net/'), ItemsModule],
+  imports: [LoginModule, SignupModule, MongooseModule.forRoot('mongodb+srv://backend-test:Test123@backend-test.dayvnzs.mongodb.net/'), ItemsModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
